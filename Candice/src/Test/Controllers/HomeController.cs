a7943eab657883269df0946a4e004cc80f54766f
YinYiPeng <18845296347@163.com>
2017-02-05 16:35:28 +0800

    Add Test Project
    Add Test Project

---
+++ Candice/src/Test/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment hostingEnv;
+        public HomeController(IHostingEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file, bool notUsed = false)
+        {
+            if (file != null)
+            {
+                var guidFileName = $"{Guid.NewGuid()}.csv";
+                //ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');   //获取文件名
+                var filePath = hostingEnv.WebRootPath + $@"\{guidFileName}";
+                using (FileStream fs = System.IO.File.Create(filePath))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
